37d93420a2d91502bea1972c2bd62956918bf38a
kielllll <eatarranza@gmail.com>
2023-12-13 22:56:24 +0800

    chore: setup `react-query`

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import { RouterProvider } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import { routes } from './router'
 
+const queryClient = new QueryClient()
+
 const App: React.FC = () => {
-  return <RouterProvider router={routes} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={routes} />
+    </QueryClientProvider>
+  )
 }
 
 export default App
